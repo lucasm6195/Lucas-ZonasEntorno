728302f01c4208a2a18314acfcc797972d1fcbbf
Owni <120651078+mano6195@users.noreply.github.com>
2024-07-23 13:38:51 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -61,4 +61,14 @@ AddEventHandler('eliminarZonaEntorno', function(zonaId)
     end)
 end)
 
+function stringsplit(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t={}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        table.insert(t, str)
+    end
+    return t
+end
 
